961ce86fc11e2a55c2df2df8c5bd54df068fef17
ghanshyamjarvis <ghanshyam.jarvis@gmail.com>
2020-07-30 14:46:23 +0530

    first

---
+++ server.js
@@ -0,0 +1,21 @@
+const mysql = require('mysql');
+const express =require('express');
+const app = express();
+
+const port = process.env.PORT || 3307;
+
+const server = app.listen(port, function() {
+  console.log('API listening on port ' + port);
+});
+
+const connection = mysql.createConnection({
+  host:'localhost',
+  user:'root',
+  password:'password',
+});
+
+ connection.connect()
+ console.log('database connected')
+connection.end()
+console.log('database connected11')
+server.close();
